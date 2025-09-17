6abcebeed2124a7bdbe0687ec450b36f0dacbce5
giihsatiko <giovana_satiko@hotmail.com>
2025-09-17 17:55:09 -0300

    feat: new routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,30 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { darkTheme } from '@/src/theme/dark';
+import { QueryClient } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { ThemeProvider } from 'styled-components/native';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={darkTheme}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ title: 'Home', headerShown: false }} />
+            <Stack.Screen
+              name="[id]"
+              options={{
+                title: 'Detalhes',
+              }}
+            />
+          </Stack>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </SafeAreaProvider>
   );
 }
